5777eaac3965787e51e5f85f01ee5215d539d51c
Sam Ritchie <sritchie09@gmail.com>
2011-03-23 21:10:31 -0400

    node.clj up to date with current hadoop crate. Cluster launches, aside from ssh stuff.

--- project.clj
+++ project.clj
@@ -2,13 +2,32 @@
   :description "A pallet config for cascalog projects"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.cloudhoist/pallet "0.4.8"]
-                 [vmfest/vmfest "0.2.3-SNAPSHOT"]
+                 [org.cloudhoist/pallet "0.4.10"
+                  :exclusions [org.jclouds/jclouds-compute
+                               org.jclouds/jclouds-blobstore
+                               org.jclouds/jclouds-scriptbuilder
+                               org.jclouds/jclouds-aws
+                               org.jclouds/jclouds-bluelock
+                               org.jclouds/jclouds-gogrid
+                               org.jclouds/jclouds-rackspace
+                               org.jclouds/jclouds-rimuhosting
+                               org.jclouds/jclouds-slicehost
+                               org.jclouds/jclouds-terremark
+                               org.jclouds/jclouds-jsch
+                               org.jclouds/jclouds-log4j
+                               org.jclouds/jclouds-enterprise]]
+                 [org.cloudhoist/pallet-crates-standalone "0.4.0"]
                  [cascalog "1.7.0-SNAPSHOT"]
+                 [org.jclouds/jclouds-all "1.0-beta-9b"]
+                 [org.jclouds.driver/jclouds-jsch "1.0-beta-9b"]
+                 [org.jclouds.driver/jclouds-log4j "1.0-beta-9b"]
+                 [org.jclouds.driver/jclouds-enterprise "1.0-beta-9b"]
+                 [com.jcraft/jsch "0.1.42"]
+                 [log4j/log4j "1.2.14"]
                  [org.cloudhoist/hadoop "0.4.0-SNAPSHOT"]
                  [org.cloudhoist/automated-admin-user "0.4.0"]
                  [org.cloudhoist/java "0.4.0"]]
   :dev-dependencies [[swank-clojure/swank-clojure "1.2.1"]
-                     [org.cloudhoist/pallet-lein "0.4.0"]]
+                     [vmfest/vmfest "0.2.2"]]
   :repositories {"sonatype"
                  "https://oss.sonatype.org/content/repositories/releases/"})
--- src/pallet_cascalog/node.clj
+++ src/pallet_cascalog/node.clj
@@ -1,11 +1,17 @@
 (ns pallet-cascalog.node
-  (:use [pallet.core :only (defnode)]
+  (:use [pallet.thread-expr :only (for->)]
         [pallet.resource :only (phase)]
         [pallet.crate.automated-admin-user
          :only (automated-admin-user)])
   (:require pallet.compute.vmfest
+            [pallet.core :as core]
+            [pallet.compute :as compute]
             [pallet.crate.hadoop :as hadoop]
-            [pallet.crate.java :as java]))
+            [pallet.crate.java :as java])
+  (:import [java.net InetAddress]))
+
+(def service
+  (compute/compute-service-from-config-file :virtualbox))
 
 (def parallel-env {}
   #_{:algorithms
@@ -13,7 +19,9 @@
     :vmfest {:create-nodes-fn pallet.compute.vmfest/parallel-create-nodes}
     :converge-fn pallet.core/parallel-adjust-node-counts}})
 
-(def local-proxy "http://10.0.1.9:3128")
+(def local-proxy (format "http://%s:3128"
+                         (.getHostAddress
+                          (InetAddress/getLocalHost))))
 
 (def local-node-specs
   (let [default-image  {:image
@@ -21,54 +29,85 @@
                          :os-64-bit true}}]
     {:tags
      {:hadoop default-image
-      :name-node default-image
-      :job-tracker default-image}
+      :namenode default-image
+      :jobtracker default-image
+      :slavenode default-image}
      :phases {:bootstrap
-              (fn [request]
-                (pallet.resource.package/package-manager
-                 request
-                 :configure :proxy local-proxy))}
+              (phase
+               (pallet.resource.package/package-manager
+                :configure :proxy local-proxy))}
      :proxy local-proxy}))
 
 (def local-env
   (merge local-node-specs parallel-env))
 
-(defnode hadoop
-  {}
+(def hadoop-config
+  (phase
+   (java/java :jdk)
+   hadoop/install
+   (hadoop/configure "/tmp/hadoop"
+                     :namenode
+                     :jobtracker
+                     :public
+                     {})))
+
+(core/defnode hadoop
+  {:inbound-ports [50030 50060 50070]}
   :bootstrap automated-admin-user
-  :configure (phase
-              (java/java :jdk)
-              hadoop/install
-              (hadoop/configure "/tmp/hadoop/"
-                                :name-node ;; name of the node
-                                :job-tracker
-                                {}))
-  :reconfigure (phase
-                (hadoop/configure "/tmp/hadoop"
-                                  :name-node ;; name of the node
-                                  :job-tracker
-                                  {})))
+  :configure hadoop-config
+  :reconfigure hadoop-config)
+
+(defn hadoop-node
+  [tag phasemap]
+  (-> hadoop
+      (assoc-in [:tag] tag)
+      (for-> [[key phaseval] phasemap]
+             (assoc-in
+              [:phases key] phaseval))))
 
 (def name-node
-  (-> hadoop
-      (assoc-in
-       [:phases :start] (phase
-                         (hadoop/name-node "/tmp/node-name/data" )))
-      (assoc-in
-       [:tag] :name-node)))
+  (hadoop-node
+   :namenode
+   {:start-namenode (phase
+                     (hadoop/name-node "/tmp/node-name/data"))
+    :start-hdfs (phase
+                 (hadoop/data-node))
+    :start-mapred (phase
+                   (hadoop/task-tracker))}))
 
 (def job-tracker
-  (-> hadoop
-      (assoc-in
-       [:phases :start] (phase
-                         (hadoop/job-tracker)))
-      (assoc-in
-       [:tag] :job-tracker)))
+  (hadoop-node
+   :jobtracker
+   {:start-jobtracker (phase
+                       (hadoop/job-tracker))
+    :start-hdfs (phase
+                 (hadoop/data-node))
+    :start-mapred (phase
+                   (hadoop/task-tracker))}))
 
-(comment
-  (use 'pallet.core)
-  (use 'pallet.compute)
-  (def service (compute-service-from-config-file :virtualbox))
-  (use 'pallet-cascalog.node)
-  (converge {hadoop 1} :compute service :environment local-env)
-  (converge {name-node 0} :compute service :environment local-env))
+(def slave-node
+  (hadoop-node
+   :slavenode
+   {:start-hdfs (phase
+                 (hadoop/data-node))
+    :start-mapred (phase
+                   (hadoop/task-tracker))}))
+
+(defn boot-cluster
+  [nodecount]
+  (core/converge {name-node 1 job-tracker 1 slave-node nodecount}
+                 :compute service
+                 :environment local-env))
+
+(defn start-cluster
+  []
+  (core/lift [name-node job-tracker slave-node]
+             :compute service
+             :environment local-env
+             :phase [:start-namenode :start-jobtracker :start-hdfs :start-mapred]))
+
+(defn kill-cluster
+  []
+  (core/converge {name-node 0 job-tracker 0 slave-node 0}
+                 :compute service
+                 :environment local-env))
