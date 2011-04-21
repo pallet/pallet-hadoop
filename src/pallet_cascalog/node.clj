939d8953f3111c0e25447843dd85638c204c8324
Sam Ritchie <sritchie09@gmail.com>
2011-04-21 15:06:50 -0400

    Added test cluster config.

--- src/pallet_cascalog/node.clj
+++ src/pallet_cascalog/node.clj
@@ -246,6 +246,7 @@
 
 ;; EXPLAIN
 ;; TODO -- add overall cluster default hadoop properties.
+
 (defn cluster-spec [ip-type nodedefs & {:keys [base-machine-spec base-props]
                                         :or [base-machine-spec {}
                                              base-props {}]
@@ -253,84 +254,25 @@
   (merge options {:ip-type ip-type
                   :nodedefs nodedefs}))
 
-(def example-cluster-spec
-  (cluster-spec :private
-                {:namenode    (hadoop-node [:namenode :slavenode])
-                 :jobtracker  (hadoop-node [:jobtracker :slavenode])
-                 :slaves      (slave-node 1)
-                 :spot-slaves (slave-node 5 :base-spec {:spot-price (float 0.03)})}
-                :base-props {:hadoop-env {:JAVA_LIBRARY_PATH "path/to/java/libs"
-                                          :LD_LIBRARY_PATH    "some/required/libs"}
-                             :mapred-site {:mapred.child.java.opts "-Djava.library.path=path/to/java/libs"
-                                           :mapred.child.env       "LD_LIBRARY_PATH=some/required/libs"}}))
+(defn test-cluster [ip-type nodecount]
+  (cluster-spec ip-type
+                {:master (hadoop-node [:namenode :jobtracker])
+                 :slaves (slave-node nodecount)}
+                :base-machine-spec {:os-family :ubuntu
+                                    :os-version-matches "10.10"
+                                    :os-64-bit true}
+                :base-props {:mapred-site {:maped.tasks.timeout 300000
+                                           :mapred.tasktracker.map.tasks.maximum 20
+                                           :mapred.tasktracker.reduce.tasks.maximum 20}}))
 
-;; ### Actual Job Run
-;;
-;; First, a test cluster. I ran this last night, and all worked wonderfully.
-
-(def-phase-fn install-redd-configs
-  "Takes pairs of strings -- the first should be a filename in the
-  `reddconfig` bucket on s3, and the second should be the unpacking
-  directory on the node."
-  [& filename-localpath-pairs]
-  (for [[remote local] (partition 2 filename-localpath-pairs)]
-    (rd/remote-directory local
-                         :url (str "https://reddconfig.s3.amazonaws.com/" remote)
-                         :unpack :tar
-                         :tar-options "xz"
-                         :strip-components 2
-                         :owner h/hadoop-user
-                         :group h/hadoop-user)))
-
-
-(def fw-path "/usr/local/fwtools")
-(def native-path "/home/hadoop/native")
-(def serializers
-  (apply str (interpose "," ["forma.FloatsSerialization"
-                             "forma.IntsSerialization"
-                             "cascading.tuple.hadoop.BytesSerialization"
-                             "cascading.tuple.hadoop.TupleSerialization"
-                             "org.apache.hadoop.io.serializer.WritableSerialization"
-                             "org.apache.hadoop.io.serializer.JavaSerialization"])))
-
-(def-phase-fn config-redd
-  "This phase installs the two files that we need to make redd run
-  with gdal! We do need some better documentation, here."
-  []
-  (package/package "libhdf4-dev")
-  (install-redd-configs
-   "FWTools-linux-x86_64-4.0.0.tar.gz" fw-path
-   "linuxnative.tar.gz" native-path))
-
-(defn forma-cluster [ip-type nodecount]
-  (let [lib-path (str fw-path "/usr/lib")]
-    (cluster-spec ip-type
-                  {:master (hadoop-node [:namenode :jobtracker])
-                   :slaves (slave-node nodecount)}
-                  :base-machine-spec {:image-id "us-east-1/ami-321eed5b"
-                                      :hardware-id "cc1.4xlarge"
-                                      :spot-price (float 1.60)}
-                  :base-props {:hadoop-env {:JAVA_LIBRARY_PATH native-path
-                                            :LD_LIBRARY_PATH lib-path}
-                               :hdfs-site {:dfs.data.dir "/mnt/dfs/data"
-                                           :dfs.name.dir "/mnt/dfs/name"}
-                               :core-site {:io.serializations serializers}
-                               :mapred-site {:maped.tasks.timeout 300000
-                                             :mapred.reduce.tasks 
-                                             :mapred.tasktracker.map.tasks.maximum 7
-                                             :mapred.tasktracker.reduce.tasks.maximum 7
-                                             :mapred.child.java.opts (str "-Djava.library.path=" native-path " -Xmx512m")
-                                             :mapred.child.env (str "LD_LIBRARY_PATH=" lib-path)}})))
-
-(defn forma-boot [node-count]
-  (let [cluster (forma-cluster :private node-count)]
+(defn test-boot [node-count]
+  (let [cluster (test-cluster :private node-count)]
     (do (boot-cluster cluster :compute env/ec2-service :environment env/remote-env)
-        (lift-cluster cluster [config-redd
-                               :start-namenode
+        (lift-cluster cluster [:start-namenode
                                :start-hdfs
                                :start-jobtracker
                                :start-mapred]
                       :compute env/ec2-service :environment env/remote-env))))
 
-(defn forma-kill []
-  (kill-cluster (forma-cluster :private 0) :compute env/ec2-service :environment env/remote-env))
+(defn test-kill []
+  (kill-cluster (test-cluster :private 0) :compute env/ec2-service :environment env/remote-env))
