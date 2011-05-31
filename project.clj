518748c31e434c312541043ec12b0141a5c620b1
Sam Ritchie <sritchie09@gmail.com>
2011-05-31 02:17:15 -0400

    making exclusions for dev version.

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.cloudhoist/pallet "0.5.0"]
-                 [org.cloudhoist/hadoop "0.4.0-SNAPSHOT"]
+                 [org.cloudhoist/hadoop "0.5.0-SNAPSHOT"]
                  [org.cloudhoist/automated-admin-user "0.5.0"]
-                 [org.cloudhoist/java "0.5.0"]]
+                 [org.cloudhoist/java "0.5.0"]
+                 [org.jclouds/jclouds-aws "1.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure/swank-clojure "1.2.1"]])
