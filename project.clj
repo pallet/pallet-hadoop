df6435108b2b6515911301df23fbec5c45005215
Sam Ritchie <sritchie09@gmail.com>
2011-05-31 01:51:55 -0400

    upgraded project.clj.

--- project.clj
+++ project.clj
@@ -7,24 +7,8 @@
                  "https://oss.sonatype.org/content/repositories/snapshots/"}  
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.cloudhoist/pallet "0.4.17"
-                  :exclusions [org.jclouds/jclouds-compute
-                               org.jclouds/jclouds-blobstore
-                               org.jclouds/jclouds-scriptbuilder
-                               org.jclouds/jclouds-aws
-                               org.jclouds/jclouds-bluelock
-                               org.jclouds/jclouds-gogrid
-                               org.jclouds/jclouds-rackspace
-                               org.jclouds/jclouds-rimuhosting
-                               org.jclouds/jclouds-slicehost
-                               org.jclouds/jclouds-terremark
-                               org.jclouds/jclouds-jsch
-                               org.jclouds/jclouds-log4j
-                               org.jclouds/jclouds-enterprise]]
-                 [org.jclouds/jclouds-all "1.0-beta-9c"]
-                 [com.jcraft/jsch "0.1.42"]
+                 [org.cloudhoist/pallet "0.5.0"]
                  [org.cloudhoist/hadoop "0.4.0-SNAPSHOT"]
-                 [org.cloudhoist/automated-admin-user "0.4.0"]
-                 [org.cloudhoist/java "0.4.0"]
-                 [log4j/log4j "1.2.14"]]
+                 [org.cloudhoist/automated-admin-user "0.5.0"]
+                 [org.cloudhoist/java "0.5.0"]]
   :dev-dependencies [[swank-clojure/swank-clojure "1.2.1"]])
