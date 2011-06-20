0f3dd05034989030bc72d01075bbe377da2f50cf
Sam Ritchie <sritchie09@gmail.com>
2011-06-19 23:53:13 -0400

    upgraded deps.

--- project.clj
+++ project.clj
@@ -5,13 +5,13 @@
                  "https://oss.sonatype.org/content/repositories/releases/"}  
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.cloudhoist/pallet "0.5.1"]
+                 [org.cloudhoist/pallet "0.6.1"]
                  [org.cloudhoist/hadoop "0.5.1-SNAPSHOT"]
                  [org.cloudhoist/java "0.5.1"]
                  [org.cloudhoist/automated-admin-user "0.5.0"]]
   :dev-dependencies [[swank-clojure/swank-clojure "1.2.1"]
-                     [org.jclouds/jclouds-all "1.0-beta-9c"]
-                     [org.jclouds.driver/jclouds-jsch "1.0-beta-9c"]
-                     [org.jclouds.driver/jclouds-log4j "1.0-beta-9c"]
+                     [org.jclouds/jclouds-all "1.0.0"]
+                     [org.jclouds.driver/jclouds-jsch "1.0.0"]
+                     [org.jclouds.driver/jclouds-log4j "1.0.0"]
                      [log4j/log4j "1.2.14"]
                      [vmfest/vmfest "0.2.2"]])
