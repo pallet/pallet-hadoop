5bccc891fdd6e997f7a19f62e01ecc298c131cf4
Antoni Batchelli <tbatchelli@acm.org>
2012-07-10 22:25:35 -0700

    Update to org.cloudhoist/hadoop 0.7.0-alpha2

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject pallet-hadoop "0.3.3-beta.1"
+(defproject pallet-hadoop "0.3.3-SNAPSHOT"
   :description "Pallet meets Hadoop."
   :dev-resources-path "dev"
   :repositories {"sonatype" "https://oss.sonatype.org/content/repositories/releases/"}  
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.cloudhoist/pallet "0.6.1"]
-                 [org.cloudhoist/hadoop "0.6.0"]
+                 [org.cloudhoist/pallet "0.6.8"]
+                 [org.cloudhoist/hadoop "0.7.0-alpha.2"]
                  [org.cloudhoist/java "0.5.1"]
                  [org.cloudhoist/automated-admin-user "0.5.0"]]
   :dev-dependencies [[swank-clojure/swank-clojure "1.2.1"]
