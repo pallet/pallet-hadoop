7b8ce9b908efab413bbe2d5b53349c417cbd9074
Antoni Batchelli <tbatchelli@acm.org>
2012-07-11 11:43:44 -0700

    Update dependencies to hadoop 0.7.0-alpha.2 and latest compatible pallet and jclouds.

--- project.clj
+++ project.clj
@@ -2,15 +2,14 @@
   :description "Pallet meets Hadoop."
   :dev-resources-path "dev"
   :repositories {"sonatype" "https://oss.sonatype.org/content/repositories/releases/"}  
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.cloudhoist/pallet "0.6.8"]
                  [org.cloudhoist/hadoop "0.7.0-alpha.2"]
                  [org.cloudhoist/java "0.5.1"]
                  [org.cloudhoist/automated-admin-user "0.5.0"]]
-  :dev-dependencies [[swank-clojure/swank-clojure "1.2.1"]
-                     [org.jclouds/jclouds-all "1.0.0"]
-                     [org.jclouds.driver/jclouds-jsch "1.0.0"]
-                     [org.jclouds.driver/jclouds-log4j "1.0.0"]
-                     [log4j/log4j "1.2.14"]
-                     [vmfest/vmfest "0.2.2"]])
+  :dev-dependencies [[org.jclouds/jclouds-all "1.2.2"]
+                     [org.jclouds.driver/jclouds-jsch "1.2.2"]
+                     [org.jclouds.driver/jclouds-slf4j "1.2.2"]
+                     [ch.qos.logback/logback-classic "1.0.1"]
+                     [ch.qos.logback/logback-core "1.0.1"]])
