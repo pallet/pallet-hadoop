5e4d155f203ffceb6ab105456110a3e06fc1c276
Antoni Batchelli <tbatchelli@acm.org>
2012-07-12 16:17:26 -0700

    Default to pallet 0.7.0 and clojure 1.4

--- project.clj
+++ project.clj
@@ -2,14 +2,14 @@
   :description "Pallet meets Hadoop."
   :dev-resources-path "dev"
   :repositories {"sonatype" "https://oss.sonatype.org/content/repositories/releases/"}  
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [org.cloudhoist/pallet "0.6.8"]
-                 [org.cloudhoist/hadoop "0.7.0-alpha.2"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.cloudhoist/pallet "0.7.0"]
+                 [org.cloudhoist/hadoop "0.7.0-beta.2"]
                  [org.cloudhoist/java "0.5.1"]
                  [org.cloudhoist/automated-admin-user "0.5.0"]]
-  :dev-dependencies [[org.jclouds/jclouds-all "1.2.2"]
-                     [org.jclouds.driver/jclouds-jsch "1.2.2"]
-                     [org.jclouds.driver/jclouds-slf4j "1.2.2"]
+  :dev-dependencies [[org.jclouds/jclouds-all "1.4.1"]
+                     [org.jclouds.driver/jclouds-jsch "1.4.1"]
+                     [org.jclouds.driver/jclouds-slf4j "1.4.1"]
+                     [org.cloudhoist/pallet-jclouds "1.4.0-beta.1"]
                      [ch.qos.logback/logback-classic "1.0.1"]
                      [ch.qos.logback/logback-core "1.0.1"]])
