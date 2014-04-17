ae095a68565e8fe4d9d73e992081b80d17b1fa00
Antoni Batchelli <tbatchelli@acm.org>
2014-04-17 16:09:28 -0700

    prepeare for lein-pallet-release

---
+++ profiles.clj
@@ -0,0 +1,3 @@
+{:dev {:plugins [[lein-pallet-release "0.1.6"]],
+       :pallet-release {:url "https://pbors:${GH_TOKEN}@github.com/pallet/pallet-hadoop.git",
+                        :branch "master"}}}
