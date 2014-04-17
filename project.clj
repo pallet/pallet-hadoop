c886b37087a8b3201b702356108a00ca6b2cc52b
Antoni Batchelli <tbatchelli@acm.org>
2014-04-17 16:23:06 -0700

    update dependency on pallet-jclouds, and group name to com.palletops

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject org.cloudhoist/pallet-hadoop "0.3.4-SNAPSHOT"
+(defproject com.palletops/pallet-hadoop "0.3.4-SNAPSHOT"
   :description "Pallet meets Hadoop."
   :dev-resources-path "dev"
+  :url "http://github.com/pallet/pallet-hadoop"
+  :scm {:url "git@github.com:pallet/pallet-hadoop.git"}
   :repositories {"sonatype" "https://oss.sonatype.org/content/repositories/releases/"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.cloudhoist/pallet "0.7.5"]
@@ -10,6 +12,6 @@
   :dev-dependencies [[org.apache.jclouds/jclouds-all "1.7.1"]
                      [org.apache.jclouds.driver/jclouds-jsch "1.7.1"]
                      [org.apache.jclouds.driver/jclouds-slf4j "1.7.1"]
-                     [org.cloudhoist/pallet-jclouds "1.7.0"]
+                     [com.palletops/pallet-jclouds "1.7.1"]
                      [ch.qos.logback/logback-classic "1.0.1"]
                      [ch.qos.logback/logback-core "1.0.1"]])
